41951fb3ccabae1ff8cfb122e20db9ae151317fe
mamiyakeita <134783372+mamiyakeita@users.noreply.github.com>
2025-10-25 23:28:20 +0900

    Change input directory for Eleventy configuration

--- .eleventy.js
+++ .eleventy.js
@@ -57,7 +57,7 @@ module.exports = function (eleventyConfig) {
 
   return {
     dir: {
-      input: "public",
+      input: "game-server/public",
       includes: "_layouts",
       data: "_data",
       output: "./deploy/_site",
