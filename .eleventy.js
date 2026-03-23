1bd0ef5cd9425e79aa8cd6c8510f271615711fc7
mamiyakeita <134783372+mamiyakeita@users.noreply.github.com>
2026-03-23 22:31:01 +0900

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -20,7 +20,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("game-server/assets");
 
   // game-server/js もコピーしたいならこれでOK
-  eleventyConfig.addPassthroughCopy("game-server/js");
+  eleventyConfig.addPassthroughCopy("assets");
 
   // 日付フィルタ
   eleventyConfig.addFilter("htmlDateString", (dateObj) => {
