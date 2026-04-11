c1be47e16c4623f6ac65c8bbf1d81f97764d2d10
mamiyakeita <134783372+mamiyakeita@users.noreply.github.com>
2026-04-11 14:04:07 +0900

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,20 @@
 const { DateTime } = require("luxon");
 const pluginNavigation = require("@11ty/eleventy-navigation");
-const path = require("path");
+
+/**
+ * This is the JavaScript code that sets the config for your Eleventy site
+ *
+ * You can add customizations here to define how the site builds your content
+ * Try extending it to suit your needs!
+ */
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.setTemplateFormats([
+    // Templates:
     "html",
     "njk",
     "md",
+    // Static Assets:
     "css",
     "jpeg",
     "jpg",
@@ -15,17 +23,44 @@ module.exports = function (eleventyConfig) {
   ]);
 
   eleventyConfig.addPlugin(pluginNavigation);
+  eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.setBrowserSyncConfig({ ghostMode: false });
 
-  // assets をそのままコピー
-  eleventyConfig.addPassthroughCopy("assets");
+  // Filters let you modify the content https://www.11ty.dev/docs/filters/
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+      "dd LLL yyyy"
+    );
+  });
+
+  // Build the collection of posts to list in the site
+  eleventyConfig.addCollection("posts", function (collection) {
+    /* The posts collection includes all posts that list 'posts' in the front matter 'tags'
+         - https://www.11ty.dev/docs/collections/
+      */
+
+    const coll = collection
+      .getFilteredByTag("posts")
+      .sort((a, b) => b.data.date - a.data.date);
+
+    // Adds {{ prevPost.url }} {{ prevPost.data.title }}, etc, to our njks templates
+    for (let i = 0; i < coll.length; i++) {
+      const prevPost = coll[i - 1];
+      const nextPost = coll[i + 1];
+
+      coll[i].data["prevPost"] = prevPost;
+      coll[i].data["nextPost"] = nextPost;
+    }
+
+    return coll;
+  });
 
   return {
     dir: {
-      input: "game-server",
+      input: "source",
       includes: "_layouts",
       data: "_data",
-      output: "deploy/_site"
-    }
+      output: "./deploy/_site",
+    },
   };
 };
